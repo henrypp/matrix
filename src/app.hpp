5659e54e7a3a2755d26645ace0e753f2603a829d
henrypp <support@henrypp.org>
2018-09-11 15:53:46 +0600

    bump version

--- src/app.hpp
+++ src/app.hpp
@@ -7,8 +7,8 @@
 
 #define APP_NAME L"Matrix Screensaver"
 #define APP_NAME_SHORT L"matrix"
-#define APP_VERSION L"1.3.1"
-#define APP_VERSION_RES 1,3,1,0
+#define APP_VERSION L"1.4"
+#define APP_VERSION_RES 1,4,0,0
 #define APP_COPYRIGHT L"(c) 2011-2018 " _APP_AUTHOR L"\r\n(c) J Brown 2003 (catch22.net). All Rights Reversed."
 
 #endif // __APP_H__
