e2e03b2726e8775d613437ac535d7e0cd222ca1e
henrypp <support@henrypp.org>
2018-09-11 15:58:55 +0600

    upd uninstall script

--- bin/uninstall.bat
+++ bin/uninstall.bat
@@ -1,6 +1,17 @@
 @echo off
 
-del /f /q "%windir%\system32\matrix.scr"
-del /f /q "%windir%\system32\matrix.ini"
+if exist "%windir%\system32\matrix.scr" (
+	del /f /q "%windir%\system32\matrix.scr"
+	if errorlevel 1 goto error
+)
 
+if exist "%windir%\system32\matrix.ini" (
+	del /f /q "%windir%\system32\matrix.ini"
+	if errorlevel 1 goto error
+)
+
+exit
+
+:error
+echo ERROR: Please run this script with admin rights...
 pause
