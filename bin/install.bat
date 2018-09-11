18e395e749ef6086b1f60add1f5e7806aba55e8e
henrypp <support@henrypp.org>
2018-09-11 15:53:54 +0600

    upd install script

--- bin/install.bat
+++ bin/install.bat
@@ -1,6 +1,19 @@
 @echo off
 
-copy /y /v "%~dp0matrix.scr" "%windir%\system32\matrix.scr"
-copy /y /v "%~dp0matrix.ini" "%windir%\system32\matrix.ini"
+rem Copy required files...
+@copy /y "%~dp0matrix.scr" "%windir%\system32\matrix.scr"
 
+if errorlevel 1 goto error
+
+if not exist "%windir%\system32\matrix.ini" (
+	@copy /y "%~dp0matrix.ini" "%windir%\system32\matrix.ini"
+)
+
+rem Open screesaver dialog...
+rundll32.exe shell32.dll,Control_RunDLL desk.cpl,ScreenSaver,@ScreenSaver
+
+exit
+
+:error
+echo ERROR: Please run this script with admin rights...
 pause
