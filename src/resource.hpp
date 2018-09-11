df24feaaaac2df0f07f33c9a2dd5e9e49334b516
henrypp <support@henrypp.org>
2018-09-11 11:44:28 +0600

    cosmetics

--- src/resource.hpp
+++ src/resource.hpp
@@ -10,28 +10,29 @@
 
 // Settings Dlg
 #define IDC_PREVIEW 100
-#define IDC_START 101
-#define IDC_CLOSE 102
+#define IDC_ABOUT 101
+#define IDC_RESET 102
+#define IDC_CLOSE 103
 
-#define IDC_DENSITY_CTRL 103
-#define IDC_DENSITY 104
-#define IDC_DENSITY_RANGE 105
-#define IDC_AMOUNT_CTRL 106
-#define IDC_AMOUNT 107
-#define IDC_AMOUNT_RANGE 108
-#define IDC_SPEED_CTRL 109
-#define IDC_SPEED 110
-#define IDC_SPEED_RANGE 111
-#define IDC_HUE_CTRL 112
-#define IDC_HUE 113
-#define IDC_HUE_RANGE 114
-#define IDC_RANDOMIZECOLORS_CHK 115
+#define IDC_DENSITY_CTRL 104
+#define IDC_DENSITY 105
+#define IDC_DENSITY_RANGE 106
+#define IDC_AMOUNT_CTRL 107
+#define IDC_AMOUNT 108
+#define IDC_AMOUNT_RANGE 109
+#define IDC_SPEED_CTRL 110
+#define IDC_SPEED 111
+#define IDC_SPEED_RANGE 112
+#define IDC_HUE_CTRL 113
+#define IDC_HUE 114
+#define IDC_HUE_RANGE 115
+#define IDC_RANDOMIZECOLORS_CHK 116
 
 // Bitmaps
 #define IDB_GLYPH 1
 
 // Cursors
-#define IDC_CURSOR 116
+#define IDC_CURSOR 117
 
 // Icons
 #define IDI_MAIN 100
