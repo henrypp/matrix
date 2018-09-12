0be460425da9450aa90bdc21d5fab2ba8c323a62
henrypp <support@henrypp.org>
2018-09-12 10:53:59 +0600

    cosmetics

--- bin/install.bat
+++ bin/install.bat
@@ -4,25 +4,35 @@ set "MATRIX_PATH=%~dp0matrix.scr"
 set "MATRIX_PATH_DEST=%windir%\system32\matrix.scr"
 
 if not exist "%MATRIX_PATH%" (
+
 	echo ERROR: "matrix.scr" not found.
 	pause
+
 ) else (
+
 	reg add hklm /f>nul 2>&1
 
 	if errorlevel 1 (
+
 		echo ERROR: Please run this script with admin rights...
 		pause
+
 	) else (
+
 		rem Copy required files...
 		copy /y %MATRIX_PATH% %MATRIX_PATH_DEST%
 
 		rem Set as default screensaver...
 		if exist %MATRIX_PATH_DEST% (
+
 			reg add "HKCU\Control Panel\Desktop" /v "ScreenSaveActive" /t REG_SZ /d "1" /f
 			reg add "HKCU\Control Panel\Desktop" /v "SCRNSAVE.EXE" /t REG_SZ /d "%MATRIX_PATH_DEST%" /f
+
 		)
 
 		rem Open screesaver dialog...
 		rem rundll32.exe shell32.dll,Control_RunDLL desk.cpl,ScreenSaver,@ScreenSaver
+
 	)
+
 )
--- bin/uninstall.bat
+++ bin/uninstall.bat
@@ -1,18 +1,21 @@
 @echo off
 
-if exist "%windir%\system32\matrix.scr" (
-	del /f /q "%windir%\system32\matrix.scr"
-	if errorlevel 1 goto error
+reg add hklm /f>nul 2>&1
+
+if errorlevel 1 (
+
+	echo ERROR: Please run this script with admin rights...
+	pause
+
+
+) else (
+
+	if exist "%windir%\system32\matrix.scr" (
+		del /f /q "%windir%\system32\matrix.scr"
+	)
+
+	if exist "%windir%\system32\matrix.ini" (
+		del /f /q "%windir%\system32\matrix.ini"
+	)
+
 )
-
-if exist "%windir%\system32\matrix.ini" (
-	del /f /q "%windir%\system32\matrix.ini"
-	if errorlevel 1 goto error
-)
-
-exit
-
-:error
-
-echo ERROR: Please run this script with admin rights...
-pause
